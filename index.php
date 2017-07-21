d32f972a0b3ec5f94661292eeb98ae2c4297b84a
Andrey Zub <and.zub.u@gmail.com>
2017-07-21 17:15:29 +0300

    did task 3

--- index.php
+++ index.php
@@ -2,11 +2,16 @@
 echo 'Задача 2 <br>';
 $arr = array();
 echo '<table style="border: solid 1px #ddd; padding: 4px; background: #ddd"><tr>';
-for ($i=0; $i<=1000; $i++){
-  echo '<td style="background: #fff">' . $i . 'x' . $i . '=' . $i * $i . '</td>';
+for ($i=0; $i<=1000; $i++) {
+    echo '<td style="background: #fff">' . $i . 'x' . $i . '=' . $i * $i . '</td>';
 }
 echo '</tr></table>';
-echo '<hr>';
-echo 'Задача 3';
 
- ?>
+echo '<hr>';
+echo 'Задача 3<br>';
+$arrayRand = array();
+for ($i=1; $i<=1000; $i++) {
+  echo $arrayRand[] = rand() . ' ';
+}
+echo '<br>';
+print_r(array_count_values($arrayRand));
