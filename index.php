e70616acce46030e5afac93da39988bf9b8f630e
Andrey Zub <and.zub.u@gmail.com>
2017-07-21 12:22:39 +0300

    did task 2

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo 'Задача 2 <br>';
+$arr = array();
+echo '<table style="border: solid 1px #ddd; padding: 4px; background: #ddd"><tr>';
+for ($i=0; $i<=1000; $i++){
+  echo '<td style="background: #fff">' . $i . 'x' . $i . '=' . $i * $i . '</td>';
+}
+echo '</tr></table>';
+echo '<hr>';
+echo 'Задача 3';
+
+ ?>
